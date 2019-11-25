846bd344e63e961f86f1b133a3388f1519473f15
Murat J. Isidore <rmurat2@yahoo.fr>
2019-11-24 20:38:40 -0500

    add nav bar, if, for and switch

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -2,19 +2,32 @@ import { Component, Input } from '@angular/core'
 
 @Component({
     selector: 'event-thumbnail',
-    template: `
-    <h2>{{ event.name }}</h2>
-    <div class="well hoverwell thumnail">
-      <div>Date: {{ event.date }}</div>
-      <div>Time: {{ event.time }}</div>
-      <div>Price: \${{ event.price }}</div>
-      <div>
-        <span>Location: {{ event.location.address }}</span>
-        <span class="pad-left">{{ event.location.city }}, {{ event.location.country }}</span>
+    template: `        
+    <div class="well hoverwell thumbnail">
+      <h2>{{ event?.name }}</h2>
+      <div>Date: {{ event?.date }}</div>
+      <div [ngSwitch]="event?.time" [ngClass]="greenStartTimeClass()">
+        Time: {{ event?.time }}
+        <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+        <span *ngSwitchDefault>(Normal Start)</span>
+      </div>
+      <div>Price: \${{ event?.price }}</div>
+      <div *ngIf="event?.location">
+        <span>Location: {{ event?.location?.address }}</span>
+        <span class="pad-left">{{ event?.location?.city }}, {{ event?.location?.country }}</span>
+      </div>
+      <div *ngIf="event?.onlineUrl">
+        Online URL: {{ event?.onlineUrl }}
       </div>
     </div>
   `,
   styles: [`
+    .green { color: #003300!important;}
+    .bold { font-weight: bold;}
+    .thumbnail {
+      min-height: 210px;
+    }
     .pad-left { padding-left: 10px;}
     .well div { color: #bbb;}
   `]
@@ -22,4 +35,10 @@ import { Component, Input } from '@angular/core'
 })
 export class EventThumbnailComponent {
     @Input() event: any
+
+    greenStartTimeClass() {
+      if(this.event && this.event.time === "8:00 am")
+        return "green bold"
+      return ""
+    }
 }
